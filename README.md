5ca965b40420f8197195223351f237fc2c84c7a6
sayordebbarma <sayorofficial8543@gmail.com>
2026-03-04 21:48:41 +0530

    User model for database

---
+++ README.md
@@ -0,0 +1,22 @@
+# BOB Fitness
+
+A modern gym website project built with web technologies.
+
+## Overview
+
+This repository contains the source code for the BobFitness gym website.
+
+## Getting Started
+
+Instructions for setting up the project locally will be added soon.
+
+## Features
+
+- Responsive design
+- Member information
+- Class schedules
+- Contact information
+
+## License
+
+Sayor Debbarma
---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters'],
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: ['member', 'admin'],
+    default: 'member',
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  }
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// Method to compare passwords on login
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
